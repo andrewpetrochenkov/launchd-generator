dd39536c3c9096c08b3fde19c46a90db0d3cf8ef
andrewp-as-is <russianidiot.github@gmail.com>
2020-12-04 00:02:39 +0300

    2020:12:04 00:02

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='launchd-generator',
+    version='2020.12.3',
     install_requires=open('requirements.txt').read().splitlines(),
     packages=setuptools.find_packages(),
     scripts=['scripts/launchd-generator']
